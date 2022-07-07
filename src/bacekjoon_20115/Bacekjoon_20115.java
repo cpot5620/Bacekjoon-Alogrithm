870d5a525137df1034469e14eef74d82322e915c
cpot5620 <50602742+cpot5620@users.noreply.github.com>
2022-07-07 16:38:58 +0900

    백준 07/07
    백준11047 - 동전0
    백준11399 - ATM
    백준20115 - 에너지드링크
    백준20300 - 서강근육맨

---
+++ src/bacekjoon_20115/Bacekjoon_20115.java
@@ -0,0 +1,23 @@
+package bacekjoon_20115;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Bacekjoon_20115 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i =0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        double answer = arr[n-1];
+        for(int i =0; i<n-1; i++){
+            answer += (double)arr[i] / 2;
+        }
+
+        System.out.println(answer);
+    }
+}
